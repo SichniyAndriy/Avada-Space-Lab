dc475faaf4661a93c534528e370ef5f1308f02d1
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-11-21 02:36:11 +0200

    Initialize Task-03-Git project with Java classes (`Cat`, `Main`) and `.gitignore` file

---
+++ Task-03-Git/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        printGreeting("Software greeting for you");
+        System.out.println("Hello");
+        printName("User");
+    }
+
+    public static void printName(String name) {
+        System.out.println(name);
+    }
+
+    public static void printGreeting(String greeting) {
+        System.out.println(greeting);
+    }
+}
---
+++ Task-03-Git/src/model/Cat.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Cat {
+    private String name;
+
+    public void doSound() {
+        System.out.println("МЯУ");
+    }
+}
